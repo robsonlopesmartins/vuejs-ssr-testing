52a2f3cf3671465a2cdd071aee74403dbc1399ee
Robson Lopes <robson.97.lopes@gmail.com>
2017-08-13 23:49:05 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <h1>Teste simples de listagem</h1>
+
+    <div class="list" v-for="d in data">
+      <ul>
+        <li>
+          Name: {{ d.name }}
+        </li>
+        <li>
+          E-mail: {{ d.email }}
+        </li>
+        <li>
+          Company: {{ d.name }}
+        </li>
+      </ul>
+      <hr>
+    </div>
+  </ul>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    asyncData () {
+      return axios.get('http://104.236.225.42/teste.json')
+        .then((res) => {
+          return { data: res.data.data }
+        })
+    }
+  }
+</script>
